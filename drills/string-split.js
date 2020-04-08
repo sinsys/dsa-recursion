f6b38ed4b509f7fb56602bf3b59faff538f9d439
sinsys <nicholas.hazel@gmail.com>
2020-04-07 19:47:08 -0700

    Completed string-split

---
+++ drills/string-split.js
@@ -0,0 +1,25 @@
+// Write a recursive function that splits a string based on a separator (similar to String.prototype.split). Don't use JS array's split function to solve this problem.
+
+// Input: 02/20/2020 
+// Output: ["02", "20", "2020"]
+
+const stringSplit = (string, array, delimiter) => {
+
+  let str = string.trim();
+  let words = array || [];
+  let index = str.indexOf(delimiter);
+
+  // Base case
+  if (index < 0 ) {
+    words.push(str);
+    return words;
+  }
+
+  // General case
+  words.push(str.substr(0, index));
+  return stringSplit(str.slice(index + 1), words, delimiter);
+}
+
+module.exports = {
+  stringSplit
+}
